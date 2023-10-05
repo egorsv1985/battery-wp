c164ef30afabb4ecc74ea5b00e3a60311690d0a3
egorsv1985 <egorvs2009@gmail.com>
2023-10-05 15:45:35 +0300

    fix

--- wp-content/themes/battery/index.php
+++ wp-content/themes/battery/index.php
@@ -4,3 +4,28 @@ Template Name: Шаблон главной страницы
 */
 
 get_header(); ?>
+<main class="main">
+	<section class="screen1">
+		<div class="screen1__container">
+			<div class="screen1__wrapper">				
+				<h1 class="screen1__maintitle"><?php the_field('title'); ?></h1>
+				<p class="screen1__text"><?php the_field('text'); ?></p>
+				<div class="screen1__button button button_fill">
+					<a href="contact.html" class="button__text">contact us</a>
+				</div>
+			</div>
+		</div>
+		<div class="screen1__background">
+			<div class="scene">
+				<div data-depth="0.3">
+					<div class="screen1__img">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/icons/g12.svg" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+</main>
