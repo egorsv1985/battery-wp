1b633a6ec35d9b1dce60634d731d05784886340c
egorsv1985 <egorvs2009@gmail.com>
2023-11-16 16:54:59 +0300

    added pagination

--- wp-content/themes/battery/sections_templates/info.php
+++ wp-content/themes/battery/sections_templates/info.php
@@ -4,11 +4,13 @@
 		<div class="info__news news">
 
 			<?php
-			$args = array(
-				'post_type' => 'post',
-				'category_name' => 'info',
-				'posts_per_page' => 4,
+			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
+			$args = array(
+				'post_type'      => 'post',
+				'category_name'  => 'info',
+				'posts_per_page' => 1,
+				'paged'          => $paged,
 			);
 
 			$query = new WP_Query($args);
@@ -18,7 +20,6 @@
 			?>
 					<article class="news__item">
 						<a href="<?php the_permalink(); ?>" class="news__box">
-
 							<img src="<?php the_field('img_new_small'); ?>" alt="<?php the_title(); ?>" class="news__img" />
 							<div class="news__content">
 								<p class="news__date"><?php the_modified_date(); ?></p>
@@ -29,35 +30,33 @@
 							</div>
 						</a>
 					</article>
-			<?php
-				endwhile;
+				<?php
+				endwhile; ?>
 
-				wp_pagenavi(array('query' => $query));
+				<div class="pagination">
+					<?php
+					echo paginate_links(array(
+						'base'               => str_replace(999999999, '%#%', esc_url(get_pagenum_link(999999999))),
+						'format'             => '?paged=%#%',
+						'current'            => max(1, get_query_var('paged')),
+						'total'              => $query->max_num_pages,
+						'prev_text'          => __('>'),
+						'next_text'          => __('<'),
+						'type'               => 'list',
+						'end_size'           => 1,
+						'mid_size'           => 1,
+					));
+					?>
+				</div>
 
 
-				$args = array(
-					'screen_reader_text' => '&nbsp;',
-					'show_all'           => false,
-					'prev_next'          => true,
-					'prev_text'          => __('←'),
-					'next_text'          => __('→'),
-					'type'               => 'post',
-					'category_name' => 'info',
-					'add_args'           => false,
-					'add_fragment'       => '',
-					'end_size'     => 1,
-					'mid_size'     => 2,
-				);
 
-				$pagination = get_the_posts_pagination($args);
 
-				var_dump($pagination);
 
-				the_posts_pagination();
-			endif;
+			<? endif;
+
 			wp_reset_postdata();
 			?>
-
 		</div>
 	</div>
 </section>
